299af8218927dccf3e2416fcec1ddcafac8df503
soodoshll <soodoshll@gmail.com>
2018-06-04 16:50:26 +0800

    yasila

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 int main(){
-    while(true);
+    srand(time(NULL));
+    int a = rand()%2;
+    if (a)
+        while(true);
+    else
+        std::cout << "yasila" << std::endl;
     return 0;
 }
